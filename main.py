c1c389086f3f82f296250565494f7fe459fac64c
Ali Albayrak <l1120910@std.yildiz.edu.tr>
2023-08-22 19:17:33 +0300

    handling negative numbers
    add some checks to handle negative numbers

--- main.py
+++ main.py
@@ -3,25 +3,32 @@ from fastapi import FastAPI, HTTPException
 app = FastAPI()
 
 def prettify_number(number: int | float) -> str:
+    # Handlinge non-numeric values
     if not isinstance(number, (int, float)):
         raise ValueError("Invalid input type/format")
+    
+    # Handling negative numbers
+    sign = ""
+    if number < 0:
+        sign = "-"
+        number = abs(number)
+
+    # Handling large numbers
     if number >= 10**15:
         raise ValueError("Invalid input. Input number length is too large")
     
     if number >= 10**12:
-        return f"{number / 10**12:.1f}T"
+        return f"{sign}{number / 10**12:.1f}T"
     elif number >= 10**9:
-        return f"{number / 10**9:.1f}B"
+        return f"{sign}{number / 10**9:.1f}B"
     elif number >= 10**6:
-        return f"{number / 10**6:.1f}M"
+        return f"{sign}{number / 10**6:.1f}M"
     elif number >= 10**3:
-        return f"{number / 10**3:.1f}K"
+        return f"{sign}{number / 10**3:.1f}K"
     elif number != int(number):
-        return f"{number:.1f}"
+        return f"{sign}{number:.1f}"
     else:
-        return str(int(number))
-    
-    # TODO handle negative numbers
+        return f"{sign}{int(number)}"
 
 
 @app.get("/prettify/{number}")

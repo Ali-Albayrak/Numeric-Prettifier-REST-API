1c0f67a1759c06a546e16398abcaa13e643f0a98
Ali Albayrak <l1120910@std.yildiz.edu.tr>
2023-08-22 20:52:27 +0300

    add docstrings to document functions

--- main.py
+++ main.py
@@ -3,6 +3,21 @@ from fastapi import FastAPI, HTTPException
 app = FastAPI()
 
 def prettify_number(number: int | float) -> str:
+    """
+    Prettifies a numeric value according to specified rules.
+
+    Args:
+        number (int | float): The numeric value to be prettified.
+
+    Returns:
+        str: The prettified string version of the number.
+
+    Raises:
+        ValueError:
+            - If the input is not a valid numeric type.
+            - If input larger then 10^15.
+    """
+
     # Handlinge non-numeric values
     if not isinstance(number, (int, float)):
         raise ValueError("Invalid input type/format")
@@ -33,6 +48,20 @@ def prettify_number(number: int | float) -> str:
 
 @app.get("/prettify/{number}")
 def get_prettified_number(number: str):
+    """
+    REST API endpoint to prettify a numeric value.
+
+    Args:
+        number (str): The numeric value to be prettified.
+
+    Returns:
+        dict: A JSON response containing the prettified number.
+
+    Raises:
+        HTTPException:
+            - If the input cannot be parsed as a float.
+            - If there is an issue with the prettification process.
+    """
     try:
         parsed_number = float(number.replace(",","."))
         prettified_number = prettify_number(parsed_number)
